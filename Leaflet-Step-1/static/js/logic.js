5d3c2aee9d8834d3b733e65f2cbef377da49cc71
JJ Williams <74847091+jjwilliams11@users.noreply.github.com>
2021-04-12 16:13:38 -0400

    map is created and seen in html but not showing up

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -26,13 +26,12 @@ function createQuakes(quakeData){
 }
 
 function createMap(earthquakes){
-
-    let streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    let streetMap = L.tileLayer("http://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v8/14/4823/6160.mvt?access_token={accessToken}", {
         attribution:"© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
-        maxZoom:18,
+        maxZoom:16,
         zoomOffset: -1,
-        id: "mapbox/streets-v11",
+        id: "mapbox/streets-v8",
         accessToken: API_KEY
     
     });
@@ -48,9 +47,9 @@ function createMap(earthquakes){
 
 
     let earthquakeMap = L.map("mapid", {
-        center: [37.09, -95.71],
-        zoom: 4,
-        layers: [streetMap,earthquakes]
+        center: [37.00, -95.00],
+        zoom: 3,
+        layers: [streetMap, earthquakes]
     });
 
     L.control.layers(baseMaps, overlayMaps, {
